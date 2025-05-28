d18c43b5ceca78e4fe002114a7e105f632ef4458
paladin-2024 <cnzabb@gmail.com>
2025-05-28 18:26:42 +0300

    :art: initialize Flutter project structure and add essential configuration files

---
+++ lib/constraints/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+const Color tdRed = Color(0xFFB00020);
+const Color tdBlue = Color(0xFF1E88E5);
+
+const Color tdBlack = Color(0xFF000000);
+const Color tdGrey = Color(0xff717171);
+
+const Color tdBGColor = Color(0xFFEEEFF5);
---
+++ lib/screens/home.dart
@@ -0,0 +1,66 @@
+
+import 'package:flutter/material.dart';
+import 'package:to_do_app/constraints/colors.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: tdBGColor,
+      appBar: _buildAppBar(),
+      body: Container(
+        padding:EdgeInsets.symmetric(horizontal: 15),
+        child: Column(
+          children: [
+            Container(
+              decoration:BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.all(0),
+                  prefixIcon: Icon(
+                      Icons.search,
+                      color:tdBlack,
+                      size: 20,
+                  ),
+                  prefixIconConstraints: BoxConstraints(
+                      maxHeight: 20,
+                      maxWidth: 25
+                  ),
+
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: tdBGColor,
+      elevation: 0,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+        Icon(Icons.menu,
+          color: tdBlack,
+          size: 30,
+        ),
+        Container(
+          height: 40,
+          width: 40,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Image.asset("asseets/images/avatar.jpg")
+          ),
+        )
+      ],),
+    );
+  }
+}
