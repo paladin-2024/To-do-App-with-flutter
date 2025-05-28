5d26052e110f8e0549e4bdc5d5221b5954cb6cb1
paladin-2024 <cnzabb@gmail.com>
2025-05-28 19:17:16 +0300

    :lipstick: add ToDoItem widget for displaying individual todo items

---
+++ lib/widgets/todo_item.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class ToDoItem extends StatelessWidget {
+  
+  const ToDoItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Text('ToDo Item'),);
+  }
+}
