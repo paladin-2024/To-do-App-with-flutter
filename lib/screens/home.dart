f0b4e0d5f0d7fec724a748ac699130d32dae20fe
paladin-2024 <cnzabb@gmail.com>
2025-05-28 18:38:43 +0300

    :recycle: refactor search box into a separate widget for improved readability

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -14,32 +14,41 @@ class Home extends StatelessWidget {
         padding:EdgeInsets.symmetric(horizontal: 15),
         child: Column(
           children: [
-            Container(
-              decoration:BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: TextField(
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.all(0),
-                  prefixIcon: Icon(
-                      Icons.search,
-                      color:tdBlack,
-                      size: 20,
-                  ),
-                  prefixIconConstraints: BoxConstraints(
-                      maxHeight: 20,
-                      maxWidth: 25
-                  ),
-
-                ),
-              ),
-            )
+           searchBox(),
           ],
         ),
       ),
     );
   }
+
+  Widget searchBox(){
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      decoration:BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+            contentPadding: EdgeInsets.all(0),
+            prefixIcon: Icon(
+              Icons.search,
+              color:tdBlack,
+              size: 20,
+            ),
+            prefixIconConstraints: BoxConstraints(
+                maxHeight: 20,
+                maxWidth: 25
+            ),
+            border: InputBorder.none,
+            hintText: "Search",
+            hintStyle: TextStyle(color: tdGrey)
+        ),
+      ),
+    );
+  }
+
+
 
   AppBar _buildAppBar() {
     return AppBar(
