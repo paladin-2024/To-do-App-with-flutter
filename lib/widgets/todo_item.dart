1533709a3627a2c1168a3108170be6d78ae361d2
paladin-2024 <cnzabb@gmail.com>
2025-05-28 20:04:04 +0300

    :lipstick: update ToDoItem widget to use Material design and enhance interactivity

--- lib/widgets/todo_item.dart
+++ lib/widgets/todo_item.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:to_do_app/constraints/colors.dart';
 
 class ToDoItem extends StatelessWidget {
   
@@ -6,6 +7,45 @@ class ToDoItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text('ToDo Item'),);
+    return Container(
+      margin: EdgeInsets.only(bottom: 20),
+      child: ListTile(
+        onTap: () {
+          print('Clicked on ToDO Item.');
+        },
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+        tileColor: Colors.white,
+        leading: Icon(Icons.check_box, color: tdBlue,),
+        title: Text(
+          "Check Mail",
+          style: TextStyle(
+            fontSize: 16,
+            color: tdBlack,
+            decoration: TextDecoration.lineThrough,
+          ),
+        ),
+        trailing: Container(
+          padding: EdgeInsets.all(0),
+          margin: EdgeInsets.symmetric(vertical: 12),
+          height: 35,
+          width: 35,
+          decoration: BoxDecoration(
+            color: tdRed,
+            borderRadius: BorderRadius.circular(5),
+          ),
+          child: IconButton(
+            color: Colors.white,
+            iconSize: 18,
+            icon: Icon(Icons.delete),
+            onPressed: () {
+              print('clicked on delete icon');
+            },
+          ),
+        ),
+      )
+    );
   }
 }
